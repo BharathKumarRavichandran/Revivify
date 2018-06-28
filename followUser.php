c8075cc04a99013778ddd9ec5a15fdeba0a38e4a
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-29 02:01:53 +0530

    Added code to get own UsersActivity data

--- followUser.php
+++ followUser.php
@@ -17,6 +17,28 @@ $username = $_SESSION["username"];
 $currentUser = $_SESSION["username"];
 $tablename = "user";
 
+if($_SERVER['REQUEST_METHOD']=="GET"){
+
+	$sql = "USE Revivify;";
+	$conn->query($sql);
+
+	$stmt = $conn->prepare("SELECT * FROM $tablename WHERE username = ?;");
+	if(!$stmt){
+		echo "Error preparing statement ".htmlspecialchars($conn->error);
+	}
+	$stmt->bind_param("s",$username);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$stmt->close();	
+
+	if($result->num_rows>0){
+		while($row = $result->fetch_assoc()){
+			echo $row["UsersActivity"];
+		}
+	}	
+
+}
+
 if($_SERVER['REQUEST_METHOD']=="POST"){
 
 	$sql = "USE Revivify;";
